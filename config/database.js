2811d17dba4531e76a5df90a0e97a63888b665a6
Ivan <pysoccerp@hotmail.com>
2018-05-07 21:24:44 -0400

    oops spelled db connection wrong

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 // 27017 is the default port number.
 module.exports = {
-    database: 'mongodb://contat_admin:Musica0522@ds255308.mlab.com:55308/conta_prod'
+    database: 'mongodb://conta_admin:Musica0522@ds255308.mlab.com:55308/conta_prod'
 }
