d213fb4c29b08ae4873df7574d3b9c3b30a18fc0
Ivan <gonzalezim02@gmail.com>
2018-10-08 21:05:35 -0400

    fixed app pointing to index.html on the backend

--- app.js
+++ app.js
@@ -47,9 +47,9 @@ app.use('/api/employees', employees);
 //     res.send("Invalid Endpoint");
 // });
 
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+// app.get('*', (req, res) => {
+// 	res.sendFile(path.join(__dirname, 'public/index.html'));
+// });
 
 //Listen to port
 app.listen(port, () => {
