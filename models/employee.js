c88fb116da8516fae689838caaec6c6f706b2973
Ivan <gonzalezim02@gmail.com>
2019-05-02 23:08:45 -0400

    Separated project into front and back end

--- models/employee.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema   = mongoose.Schema;
-    
-const employeeSchema = new Schema({
-   name: {type: String, required: true},
-   phone: String
-});
-
-const Employee = module.exports = mongoose.model("Employee", employeeSchema);
